6dbaa89e3e7bacb0f28f80ed1b91ffeeedcdb864
ALEJANDRO GUTIERREZ <agutierrezg@seidor.es>
2017-12-25 19:15:47 +0200

    repositories + spring security + user's roles

--- backend/src/main/java/com/aimprosoft/timetracking/service/impl/UserServiceImpl.java
+++ backend/src/main/java/com/aimprosoft/timetracking/service/impl/UserServiceImpl.java
@@ -1,30 +1,67 @@
 package com.aimprosoft.timetracking.service.impl;
 
-import com.aimprosoft.timetracking.dao.Dao;
+import com.aimprosoft.timetracking.dao.UserRepository;
 import com.aimprosoft.timetracking.model.User;
 import com.aimprosoft.timetracking.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
-import javax.transaction.Transactional;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
 @Transactional
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService {
 
-    private Dao dao;
+    private UserRepository userRepository;
 
     @Override
-    @SuppressWarnings("unchecked")
-    public List<User> getAllUsers() {
-        return dao.findAll();
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public List<User> save(List<User> userList) {
+        return userRepository.save(userList);
+    }
+
+    @Override
+    public User findOne(Long id) {
+        return userRepository.findOne(id);
+    }
+
+    @Override
+    public boolean exists(Long id) {
+        return userRepository.exists(id);
+    }
+
+    @Override
+    public List<User> findAll(List<Long> idList) {
+        return userRepository.findAll(idList);
+    }
+
+    @Override
+    public long count() {
+        return userRepository.count();
+    }
+
+    @Override
+    public void delete(Long id) {
+        userRepository.delete(id);
+    }
+
+    @Override
+    public void deleteAll() {
+        userRepository.deleteAll();
     }
 
     @Autowired
-    @Qualifier("userDaoImpl")
-    public void setDao(Dao dao) {
-        this.dao = dao;
+    public void setUserRepository(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 }
